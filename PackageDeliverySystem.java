c601819f59aaea9292066b06dc58b6cb670661c4
Sofia M <smatias1@hawk.illinoistech.edu>
2026-04-14 13:25:01 +0000

    Added files and started the truck class need test cases

--- PackageDeliverySystem.java
+++ PackageDeliverySystem.java
@@ -1,4 +1,4 @@
-public class Code {
+public class PackageDeliverySystem {
 	public static void main(String[] args)
        System.out.println("Hello World");
 
---
+++ Truck.java
@@ -0,0 +1,14 @@
+public class Truck{
+    //attributes and constants 
+    private int truckID;
+    private double weightOfPacksOnTruck;
+    private double voulumeOfPacksOnTruck;
+
+    private static final int smallTruckLimit
+    private static final int mediumTR
+    private static final int
+
+
+
+
+}
