d229b0a874352197532cb132537f5fc5b392caa9
Sofia M <smatias1@hawk.illinoistech.edu>
2026-04-21 13:25:30 +0000

    truck class in progess need methods

--- Truck.java
+++ Truck.java
@@ -1,47 +1,43 @@
 public class Truck{
     //attributes and constants 
     private int truckID;
+    private int truckType; // this is what is needed to determine the following below
+
     private double currentWeight;// this depends on the packages added to the truck along with the volume
     private double currentVolume;
     private double maxWeight;
     private double maxVolume;//this and the max weight is determined by the truck used 
     private Arraylist<Pack> pack;
     private int hoursUsed;// this will get calculated and updated 
+    private int companiesStoppedAt;
     
-
     private static final int smallTruck=1;
     private static final int medTruck=2;
     private static final int largeTruck=3;
 
     public static int nextID=1;
 
-    /*private static final int smallTruckVolumeLimit=1000;
-    private static final int smallTruckWeightLimit=2000;
-    private static final int mediumTruckVolumeLimit=2000;
-    private static final int mediumTruckWeightLimit= 4000;
-    private static final int largeTruckVolumeLimit=4000;
-    private static final int largeTruckWeightLimit= 8000;
-*/
-
 //constructors
 public Truck (){
     setTruckID();
-    setCurrentWeight();
-    setCurrentVolume();
-    setMaxWeight();
-    setMaxVolume();
+    setTruckType(smallTruck); //the default truck is small 
+
+    setCurrentWeight(0);
+    setCurrentVolume(0);
+    
     setPack(new ArrayList<>());
-    //setHoursUsed();
+    setHoursUsed(0);
 }
 //non default constructor 
-public Truck(double newCurrentWeight, double newCurrentVolume, double newMaxWeight, double newMaxVolume){
+public Truck(int newTruckType){
     setTruckID();
-    setCurrentWeight(newCurrentWeight);
-    setCurrentVolume(newCurrentVolume);
-    setMaxWeight(newMaxWeight);
-    setMaxVolume(newMaxVolume);
-    //setPack(new ArrayList<>());
-    //setHoursUsed(newHoursUsed);
+    setTruckType(newTruckType);
+
+    setCurrentWeight(0);
+    setCurrentVolume(0);
+    
+    setPack(new ArrayList<>());
+    setHoursUsed(0);
 }
 
 //getters
@@ -57,36 +53,58 @@ public double getCurrentVolume(){
     return currentVolume;
 }
 
-public double getMaxWeight(){
-    return maxWeight;
+/*public double getMaxWeight(){  this might not be needed bc it is determined
+    return maxWeight;               by the truck type. The setter will do this 
 }
 
 public double getMaxVolume(){
     return maxVolume;
-}
+}*/
 
 public ArraryList<Package> getPack(){
     return pack;
 }
 
-/*public int getHoursUsed(){
+public int getHoursUsed(){
     return hoursUsed;
 }
-    */
 
 //setters 
 public void setTruckID(){
     truckID= nextID++;
 }
 
-/*public void setCurrentWeight(double newCurrentWeight){
+public void setTruckType(int newTruckType){
+    if(newTruckType==smallTruck){
+        truckType=smallTruck;
+        maxWeight=2000;
+        maxVolume=1000;
+    }
+    else if (newTruckType == medTruck){
+        truckType=medTruck;
+        maxWeight=4000;
+        maxVolume=2000;
+    }
+    else if(newTruckType==largeTruck){
+        truckType=largeTruck;
+        maxWeight=8000;
+        maxVolume=4000;
+    }
+    else{
+        truckType=smallTruck;
+        maxWeight=2000;
+        maxVolume=1000;
+    }
+ }
+
+public void setCurrentWeight(double newCurrentWeight){
     if(newCurrentWeight>=0){
         currentWeight=newCurrentWeight;
     }
     else{
         currentWeight=0;
     }
-    }
+}
 
 public void setCurrentVolume(double newCurrentVolume){
     if(newCurrentVolume>=0){
@@ -95,25 +113,7 @@ public void setCurrentVolume(double newCurrentVolume){
     else{
         currentVolume=0;
     }
-    }
-*/
-public void setMaxWeight(double newMaxWeight){
-    if(newMaxWeight>=0){
-        maxWeight=newMaxWeight;
-    }
-    else{//if the input is invalid, set the max weight to 0     
-        maxWeight=0;//might have to add the condictions depending on what truck is used 
-    }
-}
-
-public void setMaxVolume(double newMaxVolume){
-    if(newMaxVolume>=0){
-        maxVolume=newMaxVolume;
-    }
-    else{
-        maxVolume=0;
-    }
-    }
+ }
 
 public void setPack(ArrayList<Pack> newPack){
     if(newPack==null){
@@ -126,7 +126,7 @@ public void setPack(ArrayList<Pack> newPack){
 
 public void setHoursUsed(int newHoursUsed){
     if(newHoursUsed>=0){
-        hours=newHoursUsed;
+        hoursUsed=newHoursUsed;
     }
     else{
         hoursUsed=0;
@@ -138,8 +138,23 @@ public boolean addPackage (Package newPack){
     if(newPack==null){
         return false;
     } 
-    else if()
+    else if(newPack.getWeight()+currentWeight<maxWeight && newPack.getVolume()+currentVolume<maxVolume){
+        pack.add(newPack);
+        setCurrentWeight(currentWeight+ newPack.getWeight());
+        setCurrentVolume( currentVolume+ newPack.getVolume());
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
+public int calculateHoursUsed(){
+    hoursUsed=0;
+    for(int i=0;  ){
+
+    }
+} 
 }
+
+
