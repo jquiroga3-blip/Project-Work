3a4310959f22816e39291f6a3dfc82c871e0bdc7
Jesus Quiroga <jquiroga3@hawk.illinoistech.edu>
2026-05-07 20:09:11 +0000

    yo

--- InvalidSpecPackException.java
+++ InvalidSpecPackException.java
@@ -1,6 +1,7 @@
 public class InvalidSpecPackException extends Exception {
     public static final String DEFAULT_MESSAGE = "Invalid spec pack data provided.";
     public static final String INVALID_TIME = "Time deadline must be between 9 and 16 inclusive.";
+    
 
     public InvalidSpecPackException() {
         super(DEFAULT_MESSAGE);
